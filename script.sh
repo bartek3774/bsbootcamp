354205585e32c275802d16a37036d3b0f956987c
bartek3774 <bartek.stempien@poczta.onet.pl>
2023-01-30 18:06:33 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-NAME=$1
-COUNTRY=$2
-SHOW=$3
+echo "Today is &(date)"
 
-echo "Script from Github"
-
-if [ "$SHOW" = "true" ]; then
-  echo "Hello, $NAME $COUNTRY"
-else
-  echo "If you want to see the name, please mark the show option"
-fi
+echo $(ls -la)
